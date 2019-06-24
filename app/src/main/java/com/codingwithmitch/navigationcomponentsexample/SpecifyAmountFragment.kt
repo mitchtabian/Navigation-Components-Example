6f691badaa2b4022309ebdaa9585ba7e0fe61e96
Mitch Tabian <mitch@tabian.ca>
2019-06-24 10:49:25 -0700

    master

---
+++ app/src/main/java/com/codingwithmitch/navigationcomponentsexample/SpecifyAmountFragment.kt
@@ -0,0 +1,36 @@
+package com.codingwithmitch.navigationcomponentsexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+
+    var navController: NavController? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.send_btn -> navController!!.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+        }
+    }
+}
