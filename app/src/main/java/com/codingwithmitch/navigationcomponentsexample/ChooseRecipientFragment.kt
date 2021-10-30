b2291c747e01d22131bff1b4a0522f5c6da4422c
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2021-10-30 15:13:53 -0300

    Update
    Update some deprecated references

--- app/src/main/java/com/codingwithmitch/navigationcomponentsexample/ChooseRecipientFragment.kt
+++ app/src/main/java/com/codingwithmitch/navigationcomponentsexample/ChooseRecipientFragment.kt
@@ -38,8 +38,8 @@ class ChooseRecipientFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.next_btn -> {
-                if(!TextUtils.isEmpty(input_recipient.text.toString())){
-                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                if(!TextUtils.isEmpty(R.id.input_recipient.toString())){
+                    val bundle = bundleOf("recipient" to R.id.input_recipient.toString())
                     navController!!.navigate(
                         R.id.action_chooseRecipientFragment_to_specifyAmountFragment,
                         bundle
@@ -50,7 +50,7 @@ class ChooseRecipientFragment : Fragment(), View.OnClickListener {
                 }
             }
 
-            R.id.cancel_btn -> activity!!.onBackPressed()
+            R.id.cancel_btn -> requireActivity().onBackPressed()
         }
     }
 }
